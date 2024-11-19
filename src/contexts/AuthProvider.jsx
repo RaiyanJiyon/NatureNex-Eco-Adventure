d1e66f079c64528e832be68963f1021b471dfed7
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-11-19 08:24:28 +0600

    feat(contexts): add AuthProvider and integrated it main.jsx file

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,9 +1,49 @@
-const AuthProvider = () => {
+import { createContext, useEffect, useState } from "react";
+import { app } from "../firebase/firebase_init";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+
+const authContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    const logIn = (email, password) => {
+        setLoading(true);
+        signInWithEmailAndPassword(auth, email, password);
+    };
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    };
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setLoading(false);
+            setUser(currentUser);
+        });
+        return () => unsubscribe();
+    }, []);
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        logIn,
+        logOut
+    }
     return (
-        <div>
-            
-        </div>
+        <authContext.Provider value={authInfo}>
+
+        </authContext.Provider>
     );
 };
 
-export default AuthProvider;
+export {AuthProvider, authContext};
