cb7fe2463f4d1f8bc7844cffacad28486fce6322
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-11-20 12:14:17 +0600

    Remove dark class from all pages

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -134,9 +134,9 @@ const Register = () => {
     return (
         <section className="bg-gray-50 pt-10 pb-10">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto  lg:py-0">
-                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                             Create an account
                         </h1>
                         <div className="flex flex-col md:flex-row items-center justify-between gap-2">
@@ -156,20 +156,20 @@ const Register = () => {
                         </div>
                         <form onSubmit={handleRegisterForm} className="space-y-4 md:space-y-6">
                             <div>
-                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
-                                <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" required />
+                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Your name</label>
+                                <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Name" required />
                             </div>
                             <div>
-                                <label htmlFor="photoURL" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your photo url</label>
-                                <input type="text" name="photoURL" id="photoURL" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Photo URL" required />
+                                <label htmlFor="photoURL" className="block mb-2 text-sm font-medium text-gray-900">Your photo url</label>
+                                <input type="text" name="photoURL" id="photoURL" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Photo URL" required />
                             </div>
                             <div>
                                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                                 <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
                             </div>
                             <div>
-                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required />
                             </div>
                             <div className="flex items-center justify-between">
                                 <div className="flex items-start">
@@ -181,20 +181,20 @@ const Register = () => {
                                             type="checkbox"
                                             checked={terms}
                                             onChange={(e) => setTerms(e.target.checked)}
-                                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" />
+                                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" />
                                     </div>
                                     <div className="ml-3 text-sm">
                                         <label
                                             htmlFor="terms"
-                                            className="text-gray-500 dark:text-gray-300">
+                                            className="text-gray-500">
                                             I accept the <span className="text-[#4F95FF] font-medium">Terms and Conditions</span>
                                         </label>
                                     </div>
                                 </div>
                             </div>
-                            <button type="submit" className="w-full text-white bg-[#4F95FF] hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
-                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Already have an account? <Link to={"/auth/login"} className="font-medium text-[#4F95FF] hover:underline dark:text-primary-500">Login here</Link>
+                            <button type="submit" className="w-full text-white bg-[#4F95FF] hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center">Create an account</button>
+                            <p className="text-sm font-light text-gray-500">
+                                Already have an account? <Link to={"/auth/login"} className="font-medium text-[#4F95FF] hover:underline">Login here</Link>
                             </p>
                         </form>
                     </div>
